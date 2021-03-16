19dbd2aa5fdf551570d29687326cc89dcfef6a6e
Jonathan Freeman <freethejazz@gmail.com>
2021-03-16 15:30:25 +1100

    Failing shadow var

--- src/main/kotlin/io/freethejazz/paintmix/domain/PaintVat.kt
+++ src/main/kotlin/io/freethejazz/paintmix/domain/PaintVat.kt
@@ -1,6 +1,13 @@
 package io.freethejazz.paintmix.domain
 
+import org.optaplanner.core.api.domain.entity.PlanningEntity
+import org.optaplanner.core.api.domain.variable.InverseRelationShadowVariable
+
+@PlanningEntity
 data class PaintVat(
     var volume: Float? = null,
     var desiredBlueShade: BlueShade? = null,
+
+    @InverseRelationShadowVariable(sourceVariableName = "paintVat")
+    var assignedBuckets: MutableList<PaintBucket>? = mutableListOf()
 )
